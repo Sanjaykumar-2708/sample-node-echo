d24282c244067b38c058ef45a7d82b61111a9ac7
Sanjay Kumar Reddy <sanjudhonib77@gmail.com>
2025-08-04 16:51:51 +0530

    updated the index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello from Jenkins + Node.js!');
 });
 
-app.listen(3000, '0.0.0.0', () => {
-  console.log('Server running on port 3000');
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
