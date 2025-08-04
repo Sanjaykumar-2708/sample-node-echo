24fab6539757d04e980ced4f777b1769eb74b088
Sanjay Kumar Reddy <sanjudhonib77@gmail.com>
2025-08-04 16:50:10 +0530

    updated the index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.get('/', (req, res) => {
   res.send('Hello from Jenkins + Node.js!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
 });
+
